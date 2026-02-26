643d9eba0fcda6fc62f50847e8a07ce0d7e089a0
VijayKumarMaddu <158272006+VijayKumarMaddu@users.noreply.github.com>
2026-02-26 18:28:11 +0530

    Update LazyLoadList.js

--- src/components/LazyLoadList.js
+++ src/components/LazyLoadList.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useCallback } from "react";
+import { useState, useRef, useCallback } from "react";
 import ArticleCard from "./ArticleCard";
 
 export default function LazyLoadList({ articles }) {
